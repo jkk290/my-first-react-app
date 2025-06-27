3ce19476f003f6a8064a50d683da9004b4cba221
jkk290 <158115736+jkk290@users.noreply.github.com>
2025-06-26 16:20:45 -1000

    Update page to display hello world

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  const message = 'Hello World!'
+
+  return (
+    <>
+      <main>
+        <h1>{message}</h1>
+      </main>
+    </>
+  )
+}
+
+export default App
